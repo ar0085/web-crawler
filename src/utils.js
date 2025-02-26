679866488c8cb2ce5430e5c84fd1c9c81af4bd60
ar0085 <125201372+ar0085@users.noreply.github.com>
2025-02-26 17:05:18 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,13 +4,17 @@ const path = require("path");
 const INDEX_FILE = path.join(__dirname, "../images/index.json");
 
 function updateIndex(imageUrl, refererUrl, depth) {
-  let indexData = { images: [] };
+  let indexData = { images: [], count: 0 };
+
+  let countToSet = 0;
 
   if (fs.existsSync(INDEX_FILE)) {
     indexData = JSON.parse(fs.readFileSync(INDEX_FILE, "utf-8"));
+    countToSet = indexData.images.length;
   }
 
   indexData.images.push({ url: imageUrl, page: refererUrl, depth });
+  indexData.count = indexData.count + 1;
 
   fs.writeFileSync(INDEX_FILE, JSON.stringify(indexData, null, 4));
 }
